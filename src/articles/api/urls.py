cbd91726436c262b7232316395b1b56ed09e3499
Napadol <napadol1412@gmail.com>
2019-02-12 16:49:16 +0700

    add backend

---
+++ src/articles/api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url,include
+
+from .views import ArticleListView,ArticleDetailView
+
+urlpatterns = [
+    url('', ArticleListView.as_view()),
+    url('<pk>', ArticleDetailView.as_view()),
+]
