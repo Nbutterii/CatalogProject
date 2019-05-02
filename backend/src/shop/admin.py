9f85a2c5f682bdcc04bede1c300a027d7082125c
Napadol <napadol1412@gmail.com>
2019-05-02 16:50:50 +0700

    add image field admin site

--- backend/src/shop/admin.py
+++ backend/src/shop/admin.py
@@ -7,7 +7,7 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     
     # list_display = [field.attname for field in User._meta.fields]
-    list_display = ['id', 'image1', 'name', 'category', 'color', 'price','Owner']
+    list_display = ['id', 'image1','image2','image3','image4', 'name', 'category', 'color', 'price','Owner']
     list_filter = ('category','color')
     
 admin.site.register(Product, ProductAdmin)
